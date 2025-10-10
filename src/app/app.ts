50719a497648035099723b60b5781febe40fa0cf
MohammedOmaraa <mohamed.ali.omaraa@gmail.com>
2025-10-10 21:53:51 +0300

    make folderStructure and globalStyles

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FlowbiteService } from './core/services/flowbite/flowbite.service';
+import { initFlowbite } from 'flowbite';
+import { Navbar } from './core/components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('FreshCart');
+
+  private readonly flowbiteService = inject(FlowbiteService);
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite((flowbite) => {
+      initFlowbite();
+    });
+  }
 }
